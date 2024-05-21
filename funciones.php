1d574e0a646a03ed75a40e051d3ac8000d44abbe
APRENDIZ <APRENDIZ@SINAPRRSCFSD537.FormacionSena.RED>
2024-05-21 08:51:49 -0500

    msnu n

--- funciones.php
+++ funciones.php
@@ -20,153 +20,190 @@
 
     <body>
         
-    <section class="our_menus" id="menus">
-        <div class="container" style="width: auto;">
-            <h2 style="text-align: center;margin-bottom: 30px">DESCUBRE NUESTROS MENÚS</h2>
-            <?php
-            require('connect.php');
-            $sql = "SELECT * FROM categorias";
-            $tabla = mysqli_query($conectar, $sql);
-            $categorias = mysqli_fetch_all($tabla, MYSQLI_ASSOC);
-            $firstCategory = $categorias[0]['idcategorias'];
-            ?>
+<section class="our_menus" id="menus">
+    <div class="container" style="width: auto;">
+        <h2 style="text-align: center; margin-bottom: 30px">DESCUBRE NUESTROS MENÚS</h2>
+        <?php
+        require('connect.php');
+        $sql = "SELECT * FROM categorias";
+        $tabla = mysqli_query($conectar, $sql);
+        $categorias = mysqli_fetch_all($tabla, MYSQLI_ASSOC);
+        $firstCategory = $categorias[0]['idcategorias'];
+        ?>
 
-            <div class="menus_tabs">
-                <div class="menus_tabs_picker">
-                    <div class="row">
-                        <?php foreach ($categorias as $categoria) { ?>
-                            <div class="col">
-                                <div class="tab_category <?php echo $categoria['idcategorias'] == $firstCategory ? 'selected' : '' ?>" id="<?php echo $categoria['idcategorias'] ?>">
-                                    <?php echo $categoria['nombre'] ?>
-                                </div>
+        <div class="menus_tabs">
+            <div class="menus_tabs_picker">
+                <div class="row">
+                    <?php foreach ($categorias as $categoria) { ?>
+                        <div class="col">
+                            <div class="tab_category <?php echo $categoria['idcategorias'] == $firstCategory ? 'selected' : '' ?>" id="<?php echo $categoria['idcategorias'] ?>">
+                                <?php echo $categoria['nombre'] ?>
                             </div>
-                        <?php } ?>
-                    </div>
+                        </div>
+                    <?php } ?>
                 </div>
-                <div class="menus_tab d-flex flex-wrap">
-                    <?php
-                    $sql = "SELECT * FROM designdb.productos";
-                    $tabla = mysqli_query($conectar, $sql);
-                    $productos = mysqli_fetch_all($tabla, MYSQLI_ASSOC);
-                    ?>
-                    <?php $i = 0;
-                    foreach ($productos as $producto) { ?>
-                        <div class="menu_item tab_category_content col-lg-3 col-md-6 col-sm-12" id="<?php echo $producto['categorias_idcategorias'] ?>" style="display: none;">
-                            <div class="row py-4">
-                                <div class="col-12">
-                                    <div class="thumbnail" style="cursor:pointer ">
-                                        <div class="menu-image">
-                                            <img style="width: 150px; height: 150px" class="image-preview" src="<?php echo $producto['imagen'] ?>" alt="">
-                                        </div>
-                                        <div class="caption">
-                                            <h5><?php echo $producto['nombre'] ?></h5>
-                                            <p><?php echo $producto['descripcion'] ?></p>
-                                            <span class="menu_price"><?php echo $producto['precio'] ?></span>
-                                            <br>                                          
-                                            <button type="button" name="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#carritoModal" onclick="agregarAlCarrito(<?php echo $producto['idproductos'] ?>)">Anadir al carrito</button>
-                                           
-                                        
-                                        </div>
+            </div>
+            <div class="menus_tab d-flex flex-wrap">
+                <?php
+                $sql = "SELECT * FROM designdb.productos";
+                $tabla = mysqli_query($conectar, $sql);
+                $productos = mysqli_fetch_all($tabla, MYSQLI_ASSOC);
+                ?>
+                <?php $i = 0;
+                foreach ($productos as $producto) { ?>
+                    <div class="menu_item tab_category_content col-lg-3 col-md-6 col-sm-12" id="<?php echo $producto['categorias_idcategorias'] ?>" style="display: none;">
+                        <div class="row py-4">
+                            <div class="col-12">
+                                <div class="thumbnail" style="cursor: pointer;">
+                                    <div class="menu-image">
+                                        <img style="width: 150px; height: 150px" class="image-preview" src="<?php echo $producto['imagen'] ?>" alt="">
+                                    </div>
+                                    <div class="caption">
+                                        <h5><?php echo $producto['nombre'] ?></h5>
+                                        <p><?php echo $producto['descripcion'] ?></p>
+                                        <span class="menu_price"><?php echo $producto['precio'] ?></span>
+                                        <br>
+                                        <input type="number" id="cantidad_<?php echo $producto['idproductos'] ?>" min="1" value="1" style="width: 60px;">
+                                        <button type="button" class="btn btn-primary" onclick="agregarAlCarrito(<?php echo $producto['idproductos'] ?>)">Añadir al carrito</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <?php if (++$i % 2 == 0) {
-                            echo '<hr class="d-md-none d-sm-block"/>';
-                        } ?>
-                    <?php } ?>
-                </div>
-            </div>
-
-            <style>
-                .tab_category.selected {
-                    text-decoration: underline;
-                    cursor: pointer;
-                }
-            </style>
-
-            <script>
-                document.addEventListener("DOMContentLoaded", function() {
-                    var categories = document.querySelectorAll('.tab_category');
-                    categories.forEach(function(category) {
-                        category.addEventListener('click', function() {
-                            var categoryId = this.getAttribute('id');
-                            var products = document.querySelectorAll('.tab_category_content');
-                            products.forEach(function(product) {
-                                if (product.getAttribute('id') === categoryId) {
-                                    product.style.display = 'block';
-                                } else {
-                                    product.style.display = 'none';
-                                }
-                            });
-
-                            // Remover clase 'selected' de todas las categorías
-                            categories.forEach(function(cat) {
-                                cat.classList.remove('selected');
-                            });
-
-                            // Agregar clase 'selected' a la categoría seleccionada
-                            this.classList.add('selected');
-                        });
-                    });
-                    var firstCategory = document.querySelector('.tab_category.selected');
-                    if (firstCategory) {
-                        firstCategory.click();
-                    }
-                });
-            </script>
-        </div>       
-    </section>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#carritoModal">Ver carrito</button>
-
-    <script>
-        var carrito = JSON.parse(localStorage.getItem('carrito') || '[]');
-
-        function agregarAlCarrito(idProducto, nombre, precio, imagen) {
-            var productoEnCarrito = carrito.find(p => p.idproductos == idProducto);
-            if (!productoEnCarrito) {
-                carrito.push({ idproductos: idProducto, nombre: nombre, precio: precio, imagen: imagen });
-                localStorage.setItem('carrito', JSON.stringify(carrito));
-            }
-            mostrarCarrito();
-        }
-
-        function mostrarCarrito() {
-            var carritoHTML = '';
-            carrito.forEach(producto => {
-                carritoHTML += `
-                    <div class="row">
-                        <div class="col-4">
-                            <img style="width: 100px; height: 100px" src="${producto.imagen}" alt="${producto.nombre}">
-                        </div>
-                        <div class="col-8">
-                            <h5>${producto.nombre}</h5>
-                            <span class="menu_price">${producto.precio}</span>
-                        </div>
                     </div>
-                    <hr>
-                `;
-            });
-            var modalBody = document.querySelector('#carritoModal .modal-body');
-            modalBody.innerHTML = carritoHTML;
-        }
-    </script>
-
-<div class="modal fade" id="carritoModal" tabindex="-1" aria-labelledby="carritoModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="carritoModalLabel">Carrito</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body"></div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary">Continuar comprando</button>
-                    <button type="button" class="btn btn-danger">Finalizar compra</button>
-                </div>
+                    <?php if (++$i % 2 == 0) {
+                        echo '<hr class="d-md-none d-sm-block"/>';
+                    } ?>
+                <?php } ?>
             </div>
         </div>
+
+        <style>
+            .tab_category.selected {
+                text-decoration: underline;
+                cursor: pointer;
+            }
+        </style>
+
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                var categories = document.querySelectorAll('.tab_category');
+                categories.forEach(function(category) {
+                    category.addEventListener('click', function() {
+                        var categoryId = this.getAttribute('id');
+                        var products = document.querySelectorAll('.tab_category_content');
+                        products.forEach(function(product) {
+                            if (product.getAttribute('id') === categoryId) {
+                                product.style.display = 'block';
+                            } else {
+                                product.style.display = 'none';
+                            }
+                        });
+
+                        // Remover clase 'selected' de todas las categorías
+                        categories.forEach(function(cat) {
+                            cat.classList.remove('selected');
+                        });
+
+                        // Agregar clase 'selected' a la categoría seleccionada
+                        this.classList.add('selected');
+                    });
+                });
+                var firstCategory = document.querySelector('.tab_category.selected');
+                if (firstCategory) {
+                    firstCategory.click();
+                }
+            });
+
+            let carrito = [];
+
+            function agregarAlCarrito(idProducto) {
+                let cantidadInput = document.getElementById('cantidad_' + idProducto);
+                let cantidad = parseInt(cantidadInput.value);
+
+                if (cantidad < 1) {
+                    alert('La cantidad debe ser al menos 1.');
+                    return;
+                }
+
+                let producto = <?php echo json_encode($productos); ?>.find(p => p.idproductos == idProducto);
+
+                carrito.push({ producto: producto, cantidad: cantidad });
+
+                actualizarCarrito();
+            }
+
+            function actualizarCarrito() {
+                const carritoItems = document.getElementById('carritoItems');
+                const totalCarrito = document.getElementById('totalCarrito');
+                carritoItems.innerHTML = '';
+                let total = 0;
+
+                carrito.forEach((item, index) => {
+                    const producto = item.producto;
+                    const cantidad = item.cantidad;
+                    const subtotal = parseFloat(producto.precio) * cantidad;
+
+                    const carritoItem = document.createElement('div');
+                    carritoItem.classList.add('carrito-item');
+                    carritoItem.innerHTML = `
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="${producto.imagen}" alt="${producto.nombre}" style="width: 100%;">
+                            </div>
+                            <div class="col-6">
+                                <h5>${producto.nombre}</h5>
+                                <p>${producto.descripcion}</p>
+                                <span>Precio: $${producto.precio}</span><br>
+                                <span>Cantidad: ${cantidad}</span><br>
+                                <span>Subtotal: $${subtotal.toFixed(2)}</span>
+                            </div>
+                            <div class="col-3">
+                                <button class="btn btn-danger btn-sm" onclick="eliminarDelCarrito(${index})">Eliminar</button>
+                            </div>
+                        </div>
+                        <hr>
+                    `;
+                    carritoItems.appendChild(carritoItem);
+                    total += subtotal;
+                });
+
+                totalCarrito.textContent = total.toFixed(2);
+            }
+
+            function eliminarDelCarrito(index) {
+                carrito.splice(index, 1);
+                actualizarCarrito();
+            }
+        </script>
     </div>
+</section>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#carritoModal">Ver carrito</button>
+
+<!-- Modal -->
+<div class="modal fade" id="carritoModal" tabindex="-1" aria-labelledby="carritoModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="carritoModalLabel">Carrito de Compras</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <div id="carritoItems">
+                <!-- Los elementos del carrito se añadirán aquí -->
+            </div>
+            <div class="total">
+                <strong>Total: $<span id="totalCarrito">0.00</span></strong>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary">Proceder al Pago</button>
+        </div>
+    </div>
+</div>
+
+
+
     
 
     
